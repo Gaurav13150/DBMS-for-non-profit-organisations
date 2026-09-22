ad1c9595f8a85cd2fca4b739b126a1f3406ab7ab
Gaurav13150 <gauravayadav01@gmail.com>
2026-09-22 21:05:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from supabase import create_client
 from datetime import date
+import pandas as pd
 
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
